c7abcc801b7d33e5c3d3210d37caae2441b668bc
tsanevp <petertsanev7@gmail.com>
2023-06-28 23:59:30 -0700

    working on displaying results once valid location is provided

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const apiKey = secrets.apiKey;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
@@ -23,36 +24,30 @@ app.get("/contact.html", function (req, res) {
 
 app.get("/about.html", function (req, res) {
     res.send("Built using Nodejs and Expressjs");
-    // res.sendFile(__dirname + "/contact.html");    
 });
 
 app.post("/", function (req, res) {
+    // console.log(req.body);
     const query = req.body.cityName;
     const units = "imperial";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
 
     https.get(url, (response) => {
-
         response.on('data', (data) => {
             const weatherData = JSON.parse(data);
-            console.log(response.statusCode);
+
             if (weatherData.cod !== 200) {
-                // res.write("There was an error. Your status code is: " + weatherData.cod);
-                res.sendFile(__dirname + "/invalidLocation.html");
-                // res.send();
+                const invalidLocation = query;
+                res.send({ invalidLocation });
                 return;
             }
+
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
             const imageId = weatherData.weather[0].icon;
             const imageURL = "https://openweathermap.org/img/wn/" + imageId + "@2x.png";
 
-            res.setHeader('Content-Type', 'text/html');
-            res.write("The weather description is: " + description);
-            res.write("<h1>The temp is: " + temp + "</h1>");
-            res.write("<img src=" + imageURL + ">")
-            res.write("<form action=/ method=GET><button type=submit name=button>Return To Search</button></form>")
-            res.send();
+            res.send({ temp, description, imageURL});
         });
     }).on('error', (e) => {
         res.send(e);
@@ -62,14 +57,15 @@ app.post("/", function (req, res) {
 app.post("/contact.html", function (req, res) {
     const { name, emailAddress, message } = req.body;
     const url = "https://docs.google.com/forms/d/e/1FAIpQLSddoLVzy0JmAmq2P5ynYhaX8hUBJ22c3yuNFlWgt7Z13SBJ3A/formResponse?entry.225115399=" + name + "&entry.90530336=" + emailAddress + "&entry.2053798052=" + message;
+
     https.get(url, (response) => {
         const responseCode = response.statusCode;
 
         if (response.statusCode !== 200) {
-
             res.sendFile(__dirname + "/contactError.html");
             return;
         }
+
         res.sendFile(__dirname + "/contactSuccess.html");
     }).on('error', (e) => {
         res.send(e);
